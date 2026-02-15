aac4afbd1c4ba51aa8223755eac7ae55a20b8662
animeshsahoo5 <arnapurnasahoo79@gmail.com>
2026-02-15 12:40:16 +0530

    first sprint (initial setup)

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Route
+app.get("/", (req, res) => {
+  res.send("WPMS API Running...");
+});
+
+// Connect Database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
